794cc3b0775b32cb6b3abcbe0d3ce76f0df88f2d
bibibobobi <smilingcat33@gmail.com>
2022-10-11 20:41:41 +0800

    add title section

--- src/components/title-section.js
+++ src/components/title-section.js
@@ -1,38 +1,116 @@
 import styled from 'styled-components';
 import bgImg1 from '../assets/Group 593.png';
+import TitleDecoSrc from '../assets/Frame 3.png';
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 254px;
+  padding: 30px 0;
+  height: 354px;
   background: #5d2e7a;
   background-image: url(${bgImg1});
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   @media (min-width: 768px) {
-    height: 362px;
+    height: 442px;
   }
 `;
 
-const TitleWrapper = styled.div`
-  width: calc(100%-40px);
+const YearWrapper = styled.div`
+  font-weight: 700;
+  font-size: 36px;
   color: white;
+  @media (min-width: 768px) {
+    font-size: 60px;
+  }
+  @media (min-width: 1440px) {
+    font-size: 72px;
+  }
 `;
 
-// const TitleDecoLeft = styled.div`
-//   width: 40px;
-//   background-image: url(${titleDeco});
-// `;
+const EnTitleWrapper = styled.div`
+  width: 288px;
+  font-weight: 700;
+  font-size: 38px;
+  color: white;
+  border-bottom: 3px solid #f5ab1c;
+  line-height: 5rem;
+  text-align: center;
+  @media (min-width: 768px) {
+    line-height: 6rem;
+    width: 368px;
+    font-size: 46px;
+  }
+  @media (min-width: 1440px) {
+    width: 448px;
+    font-size: 60px;
+  }
+`;
 
-// const TitleDecoRight = styled.div`
-//   width: 40px;
-//   background-image: url(${titleDeco});
-//   transform: rotate(180deg);
-// `;
+const ChTitleWrapper = styled.div`
+  width: 288px;
+  font-weight: 700;
+  font-size: 40px;
+  color: white;
+  border-bottom: 3px solid #f5ab1c;
+  line-height: 5rem;
+  text-align: center;
+  @media (min-width: 768px) {
+    line-height: 6rem;
+    width: 368px;
+    font-size: 48px;
+  }
+  @media (min-width: 1440px) {
+    width: 448px;
+    font-size: 62px;
+  }
+`;
+
+const TitleDecoLeft = styled.img`
+  transform: translateY(-5px) rotate(180deg);
+  width: 95px;
+  @media (min-width: 768px) {
+    transform: translateY(-10px) rotate(180deg);
+    width: 100px;
+  }
+  @media (min-width: 1440px) {
+    width: 120px;
+  }
+`;
+
+const TitleDecoRight = styled.img`
+  transform: translateY(-5px);
+  width: 95px;
+  @media (min-width: 768px) {
+    transform: translateY(-10px);
+    width: 100px;
+  }
+  @media (min-width: 1440px) {
+    width: 120px;
+  }
+`;
+
+const LogoWrapper = styled.div`
+  font-weight: 700;
+  font-size: 40px;
+  color: white;
+  background: #e51731;
+  text-align: center;
+  margin-top: 40px;
+`;
 
 const TitleSection = () => {
   return (
     <Wrapper>
-      <TitleWrapper>2022</TitleWrapper>
-      <TitleWrapper>FIFA World Cup</TitleWrapper>
-      <TitleWrapper>世足賽觀賽重點</TitleWrapper>
+      <YearWrapper>
+        <TitleDecoLeft src={TitleDecoSrc} />
+        <span> 2022 </span>
+        <TitleDecoRight src={TitleDecoSrc} />
+      </YearWrapper>
+      <EnTitleWrapper>FIFA World Cup</EnTitleWrapper>
+      <ChTitleWrapper>世足賽觀賽重點</ChTitleWrapper>
+      <LogoWrapper>LOGO</LogoWrapper>
     </Wrapper>
   );
 };
